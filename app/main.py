159452c975f8dfaee50434c94240a58b420cbf42
Stanislaw Wegrzyn <136663088+TWAZZY98@users.noreply.github.com>
2026-02-26 21:03:19 +0100

    main menu

--- app/main.py
+++ app/main.py
@@ -1,24 +1,11 @@
-import pandas as pd
-import csvdatabase as csvd
-import stochasticprocess as sp
-import normalmodel as nm
+import gui
 
-from matplotlib import pyplot as mp
-import numpy as np
-import GUI
+VERSION:str = "Alpha"
 
 def main():
-    #g = sp.geometricbrownian(304,0.30,0.1,1,100,5)
-    #g.simplepath()
-    #g.multiplesimplepath(100)
-    numOfSim = 1000
-    days = 100
-    d =sp.predict_for_data('AAPL',days,days,0,0,numOfSim)
-    mean = nm.bar_model_data(d[0])
-    vol_and_ret = d[1]
-    GUI.brownian_sim_results(mean,numOfSim,vol_and_ret[1],vol_and_ret[0])
-    # n= sp.geometricbrownian(100,0.1,0.4,40,40)
-    # n.multiplesimplepath(100)
+    ginterface = gui.GUI(VERSION)
+    ginterface.main_menu()
+
 
 if __name__ == "__main__": 
     print(f"{__name__} is being run directly")
